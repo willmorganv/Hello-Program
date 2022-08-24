2b33c3499877a5ebf351f88dbbfbabcafc0c82d6
willmorganv <112001407+willmorganv@users.noreply.github.com>
2022-08-24 11:55:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Will Morgan");
+		System.out.println("Hello Project");
+		System.out.println("General information project"); 
+		System.out.println("8/24/2022");
+		System.out.println("CMSC 255, 004"); 	
+		System.out.println();
+		
+	}
+}
